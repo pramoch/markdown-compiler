85289044b5ea892eeff01e5a455bec2c7168544b
Pramoch Chulapong <pramoch.chulapong@scb.co.th>
2018-10-12 10:20:55 +0700

    Copy template dependency

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,44 @@
 const marked = require('marked');
 
 module.exports = function (grunt) {
+  require('load-grunt-tasks')(grunt);
+
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+    copy: {
+      template: {
+        expand: true,
+        cwd: 'template/',
+        src: ['**', '!content.html'],
+        dest: 'output/'
+      },
+    }
   });
 
   // Default task(s).
   grunt.registerTask('default', ['build']);
+
+  grunt.registerTask('build', () => {
+    build();
+  });
+
+  const build = () => {
+    // ===== Clean up output folder =====
+    grunt.file.delete('output');
+
+    // ===== Copy template's dependency =====
+    grunt.task.run('copy:template');
+
+    let template = grunt.file.read('./template/content.html');
+    template = updateSidebar(template);
+
+    const patterns = ['**/*.md', '!summary.md'];
+    const files = grunt.file.expand({ cwd: './docs' }, patterns);
+    files.map((fileName) => {
+      convertToHtml(fileName, template);
+    });
+  }
 
   const updateSidebar = (template) => {
     let sidebarMarkdown = grunt.file.read('./docs/summary.md');
@@ -22,15 +53,4 @@ module.exports = function (grunt) {
     const fullContent = template.replace('%content%', docHtml);
     grunt.file.write('./output/' + fileName.replace('.md', '.html'), fullContent);
   };
-
-  grunt.registerTask('build', () => {
-    let template = grunt.file.read('./template/content.html');
-    template = updateSidebar(template);
-
-    const patterns = ['**/*.md', '!summary.md'];
-    const files = grunt.file.expand({ cwd: './docs' }, patterns);
-    files.map((fileName) => {
-      convertToHtml(fileName, template);
-    })
-  });
 };
